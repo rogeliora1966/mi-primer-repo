b3da0f41a42e4cb29142d4d2ee106c97d6e3cfac
Rogelio Rodriguez Alonso <rralonso@gmail.com>
2022-11-23 21:03:04 -0600

    Primera version de mi calculadora

--- imc.py
+++ imc.py
@@ -1,3 +1,5 @@
+# Este es mi Programa Principal
 print ("Bienvenido al programa de calculo de IMC")
 peso = float (input ("Introduce tu peso en Kilogramos:"))
-
+altura = float (input("Introduce tu altura"))
+imc = peso / altura
