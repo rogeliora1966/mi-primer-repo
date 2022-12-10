25b4e2364a43a0db1f01ab7a14134e51a6280f44
Rogelio Rodriguez Alonso <rralonso@gmail.com>
2022-12-10 13:32:30 -0600

    Primera versión de del programa de IMC

--- imc.py
+++ imc.py
@@ -1,5 +1,27 @@
-# Este es mi Programa Principal
-print ("Bienvenido al programa de calculo de IMC")
-peso = float (input ("Introduce tu peso en Kilogramos:"))
-altura = float (input("Introduce tu altura"))
-imc = peso / altura
+print ("Este es mi Programa Principal")
+#Le pedimos el nombre y lo guardamos en un input
+# Definicion de Variables
+try:
+nombre = input ("Su nombre por favor: ")
+apellido = input ("Ingrese su Pallido: ")
+if nombre =="" :
+    print ("No puede ir el dato en blanco, debe de ingresar el nombre y el apellido")
+    exit ()
+except:
+ #   print ("solo Caracteres Alfanumericos")
+
+print (nombre)
+print ("Hola",nombre,"bienvenido al programa de calculo de IMC")
+try:
+#Se pide al edad que siempre es un entero por eso el int()
+    edad = int(input("Su edad en años sin Fracciones por favor: "))
+ #El peso es con Kilos y con fracciones,  hay que ponerle punto, es un flotante float()
+    peso = float (input ("Introduce tu peso en Kilogramos: "))
+ #La altura es en metros y con fracciones,  hay que ponerle punto, es un flotante foat()
+    altura = float (input("Introduce tu altura: "))
+except:
+    print ("Error ingrese solo numeros, y no puede ir en blanco el dato")
+    exit()
+#Calculo del Indice corporal
+imc = peso / altura **2
+print (nombre, apellido,",","tu masa corporal es de",  imc)
